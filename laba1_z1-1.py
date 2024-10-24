67d18cbccf571ff77ae51dd9512e3c30277a57a9
Dasha Saluk <95886436+dasha-y@users.noreply.github.com>
2024-10-24 15:26:22 +0300

    Delete laba1_z1-1.py

--- laba1_z1-1.py
+++
@@ -1,7 +0,0 @@
-sequence = input("Введите произвольную последовательность: ")
-vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯ"
-new_sequence = ""
-for char in sequence:
-    if char in vowels or not char.isalpha():
-        new_sequence += char
-print("Новая последовательность: ", new_sequence)
